2ecf5648390b79a4bbb8e03f813820ce3d801635
Michele Giacomini <michele.giacomini91@gmail.com>
2025-10-06 22:37:01 +0200

    iterator over bitfield values and set bit

--- src/bitfield.zig
+++ src/bitfield.zig
@@ -126,6 +126,63 @@ pub const Bitfield = struct {
         const f = F{ .bf = self };
         return f.format(writer);
     }
+
+    const Item = packed struct {
+        x: usize,
+        y: usize,
+        val: bool,
+    };
+
+    const ItemIter = struct {
+        current: Elem,
+        offset: usize,
+        index: usize,
+        bf: *const Bitfield,
+
+        pub fn next(self: *ItemIter) ?Item {
+            if (self.offset == elem_bit_width) {
+                self.offset = 0;
+                self.index += 1;
+                if (self.index == self.bf.data.len) {
+                    return null;
+                }
+                self.current = self.bf.data[self.index];
+            }
+            const total_offset = self.offset + self.index * elem_bit_width;
+            const x = @mod(total_offset, self.bf.width);
+            const y = @divTrunc(total_offset, self.bf.width);
+            if (y >= self.bf.height) {
+                return null;
+            }
+            const mask = 1 << elem_bit_width - 1;
+            const val = self.current & mask > 0;
+            self.current = self.current << 1;
+            self.offset += 1;
+            return Item{ .x = x, .y = y, .val = val };
+        }
+    };
+
+    pub fn itemIterator(self: *const Bitfield) ItemIter {
+        return ItemIter{
+            .current = self.data[0],
+            .offset = 0,
+            .index = 0,
+            .bf = self,
+        };
+    }
+
+    pub fn set(self: *Bitfield, x: usize, y: usize, val: bool) void {
+        const total_offset = y * self.width + x;
+        const index = @divTrunc(total_offset, elem_bit_width);
+        const offset = @mod(total_offset, elem_bit_width);
+        const one: Elem = 1;
+        const mask: Elem = one << @intCast(elem_bit_width - 1 - offset);
+        if (val) {
+            self.data[index] = self.data[index] | mask;
+        } else {
+            self.data[index] = self.data[index] & ~mask;
+        }
+    }
 };
 
 test "From string simple" {
@@ -241,4 +298,95 @@ test "From string - toString custom" {
     const to_s = try std.fmt.allocPrint(allocator, "{f}", .{b.formatCustom(' ', '#')});
     defer allocator.free(to_s);
     try std.testing.expectEqualStrings(s, to_s);
+}
+
+test "Simple item iterator" {
+    const s =
+        \\010
+        \\001
+        \\100
+    ;
+
+    const allocator = std.testing.allocator;
+    var b = try Bitfield.initFromString(allocator, s, .{});
+    defer b.deinit(allocator);
+    var iter = b.itemIterator();
+    var current = iter.next();
+    try std.testing.expect(current.?.val == false);
+    try std.testing.expect(current.?.x == 0);
+    try std.testing.expect(current.?.y == 0);
+    current = iter.next();
+    try std.testing.expect(current.?.val == true);
+    try std.testing.expect(current.?.x == 1);
+    try std.testing.expect(current.?.y == 0);
+    current = iter.next();
+    try std.testing.expect(current.?.val == false);
+    try std.testing.expect(current.?.x == 2);
+    try std.testing.expect(current.?.y == 0);
+    current = iter.next();
+    try std.testing.expect(current.?.val == false);
+    try std.testing.expect(current.?.x == 0);
+    try std.testing.expect(current.?.y == 1);
+    current = iter.next();
+    try std.testing.expect(current.?.val == false);
+    try std.testing.expect(current.?.x == 1);
+    try std.testing.expect(current.?.y == 1);
+    current = iter.next();
+    try std.testing.expect(current.?.val == true);
+    try std.testing.expect(current.?.x == 2);
+    try std.testing.expect(current.?.y == 1);
+    current = iter.next();
+    try std.testing.expect(current.?.val == true);
+    try std.testing.expect(current.?.x == 0);
+    try std.testing.expect(current.?.y == 2);
+    current = iter.next();
+    try std.testing.expect(current.?.val == false);
+    try std.testing.expect(current.?.x == 1);
+    try std.testing.expect(current.?.y == 2);
+    current = iter.next();
+    try std.testing.expect(current.?.val == false);
+    try std.testing.expect(current.?.x == 2);
+    try std.testing.expect(current.?.y == 2);
+}
+
+test "simple set" {
+    const s =
+        \\010
+        \\001
+        \\100
+    ;
+
+    const exp =
+        \\010
+        \\001
+        \\101
+    ;
+    const allocator = std.testing.allocator;
+    var b = try Bitfield.initFromString(allocator, s, .{});
+    defer b.deinit(allocator);
+    b.set(2, 2, true);
+    const to_s = try std.fmt.allocPrint(allocator, "{f}", .{b});
+    defer allocator.free(to_s);
+    try std.testing.expectEqualStrings(exp, to_s);
+}
+
+test "simple unset" {
+    const s =
+        \\010
+        \\001
+        \\100
+    ;
+
+    const exp =
+        \\010
+        \\001
+        \\000
+    ;
+    const allocator = std.testing.allocator;
+    var b = try Bitfield.initFromString(allocator, s, .{});
+    defer b.deinit(allocator);
+    b.set(0, 2, false);
+    const to_s = try std.fmt.allocPrint(allocator, "{f}", .{b});
+    defer allocator.free(to_s);
+    try std.testing.expectEqualStrings(exp, to_s);
 }
