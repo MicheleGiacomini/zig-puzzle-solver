0eb4b3458910a1d18d65bf82a4c6d2f35a9d1f74
Michele Giacomini <michele.giacomini91@gmail.com>
2025-10-06 22:37:08 +0200

    rotate piece

--- src/board.zig
+++ src/board.zig
@@ -27,9 +27,20 @@ const Piece = struct {
     pub fn deinit(self: *Piece, allocator: std.mem.Allocator) void {
         self.store.deinit(allocator);
     }
+
+    pub fn rotate(self: *Piece, allocator: std.mem.Allocator) !Piece {
+        var b = try bf.Bitfield.init(allocator, self.height, self.width);
+        var self_iter = self.store.itemIterator();
+        while (self_iter.next()) |elem| {
+            if (elem.val) {
+                b.set(self.height - elem.y - 1, elem.x, true);
+            }
+        }
+        return Piece.init(b);
+    }
 };
 
-const BoardErr = error{InsertCollision};
+const BoardErr = error{ InsertCollision, RemoveMismatch };
 
 const Board = struct {
     current: bf.Bitfield,
@@ -128,7 +139,7 @@ const Board = struct {
     /// Takes a smaller bitfield and inserts it at offset in self. Returns an error if self & other has any "on" bit.
     pub fn insert(self: *Board, piece: *const Piece, x: usize, y: usize) BoardErr!void {
         const Local = struct {
-            pub fn insertCeck(eSelf: Elem, ePiece: Elem) ?BoardErr {
+            pub fn check(eSelf: Elem, ePiece: Elem) ?BoardErr {
                 if (eSelf & ePiece > 0) {
                     return BoardErr.InsertCollision;
                 }
@@ -139,11 +150,27 @@ const Board = struct {
             }
         };
 
-        return iterateCheckAndApply(self, piece, x, y, BoardErr, Local.insertCeck, Local.action);
+        return iterateCheckAndApply(self, piece, x, y, BoardErr, Local.check, Local.action);
+    }
+
+    pub fn remove(self: *Board, piece: *const Piece, x: usize, y: usize) BoardErr!void {
+        const Local = struct {
+            pub fn check(eSelf: Elem, ePiece: Elem) ?BoardErr {
+                if (eSelf ^ ePiece > 0) {
+                    return BoardErr.RemoveMismatch;
+                }
+                return null;
+            }
+            pub fn action(eSelf: Elem, ePiece: Elem) Elem {
+                return eSelf & ~ePiece;
+            }
+        };
+
+        return iterateCheckAndApply(self, piece, x, y, BoardErr, Local.check, Local.action);
     }
 };
 
-test "Insert piece no collision" {
+test "Insert+remove piece no collision" {
     const s =
         \\010
         \\111
@@ -153,8 +180,8 @@ test "Insert piece no collision" {
         \\0000000000
         \\0000000000
         \\0000000000
-        \\0000100000
-        \\0001110000
+        \\0000000000
+        \\0000000000
         \\0000000000
         \\0000000000
         \\0000000000
@@ -167,12 +194,13 @@ test "Insert piece no collision" {
     var board = try Board.init(allocator, 10, 10);
     defer board.deinit(allocator);
     try board.insert(&piece, 3, 3);
+    try board.remove(&piece, 3, 3);
     const actual = try std.fmt.allocPrint(allocator, "{f}", .{board.current});
     defer allocator.free(actual);
     try std.testing.expectEqualStrings(exp, actual);
 }
 
-test "Insert piece no collision across element boundaries" {
+test "Insert+remove piece no collision across element boundaries" {
     const s =
         \\010
         \\111
@@ -184,8 +212,8 @@ test "Insert piece no collision across element boundaries" {
         \\0000000000
         \\0000000000
         \\0000000000
-        \\0001000000
-        \\0011100000
+        \\0000000000
+        \\0000000000
         \\0000000000
         \\0000000000
         \\0000000000
@@ -196,7 +224,29 @@ test "Insert piece no collision across element boundaries" {
     var board = try Board.init(allocator, 10, 10);
     defer board.deinit(allocator);
     try board.insert(&piece, 2, 5);
+    try board.remove(&piece, 2, 5);
     const actual = try std.fmt.allocPrint(allocator, "{f}", .{board.current});
+    defer allocator.free(actual);
+    try std.testing.expectEqualStrings(exp, actual);
+}
+
+test "Rotate piece" {
+    const s =
+        \\100
+        \\111
+    ;
+
+    const exp =
+        \\11
+        \\10
+        \\10
+    ;
+    const allocator = std.testing.allocator;
+    var piece = try Piece.initFromString(allocator, s, .{});
+    defer piece.deinit(allocator);
+    var rotated = try piece.rotate(allocator);
+    defer rotated.deinit(allocator);
+    const actual = try std.fmt.allocPrint(allocator, "{f}", .{rotated.store});
     defer allocator.free(actual);
     try std.testing.expectEqualStrings(exp, actual);
 }
